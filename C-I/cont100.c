59bd13f206cb60c94418637918b57b770c14a950
Carson Souza <carson.m.a.r.souza@gmail.com>
2022-05-24 16:59:25 -0300

    Projeto finalizado.

---
+++ C-I/cont100.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int i = 1;
+    while(i <= 100)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    
+    return 0;
+}
